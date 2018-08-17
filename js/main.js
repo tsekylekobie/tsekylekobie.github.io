617ac6b2001b244db7f98a0b39fa7c8c5724410f
Kyle Tse <kyletse@Kyles-MacBook-Pro.local>
2018-08-17 12:10:38 -0700

    redesign

--- js/main.js
+++ js/main.js
@@ -7,35 +7,35 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     }, 500);
 });
 
-//Button functionality
-$(function() {
-	"use strict";
-	function showText(linkId) {
-		$("#container").animate({top: "-15vh"}, {duration: 600, queue: false});
+//Button functionality for 'display' class
+// $(function() { 
+// 	"use strict";
+// 	function showText(linkId) {
+// 		$("#container").animate({top: "-15vh"}, {duration: 600, queue: false});
 		
-		$(".link").removeClass("clicked");
-		$("#" + linkId).addClass("clicked");
-		$(".display").hide();
-		$("#" + linkId + "-text").fadeIn();
+// 		$(".link").removeClass("clicked");
+// 		$("#" + linkId).addClass("clicked");
+// 		$(".display").hide();
+// 		$("#" + linkId + "-text").fadeIn();
 		
-		$(".link", this).off().on('click', function() {
-			showText($(this).attr('id'));
-		});
-		$("#" + linkId).off().on('click', function() { hideText(linkId); });
-	}
+// 		$(".link", this).off().on('click', function() {
+// 			showText($(this).attr('id'));
+// 		});
+// 		$("#" + linkId).off().on('click', function() { hideText(linkId); });
+// 	}
 	
-	function hideText(linkId) {
-		$("#container").animate({top: "0vh"}, {duration: 600, queue: false});
+// 	function hideText(linkId) {
+// 		$("#container").animate({top: "0vh"}, {duration: 600, queue: false});
 		
-		$(".link").removeClass("clicked");
-		$(".display").hide();
+// 		$(".link").removeClass("clicked");
+// 		$(".display").hide();
 		
-		$(".link", this).off().on('click', function() {
-			showText($(this).attr('id'));
-		});
-	}
+// 		$(".link", this).off().on('click', function() {
+// 			showText($(this).attr('id'));
+// 		});
+// 	}
 	
-	$(".link", this).on('click', function() {
-		showText($(this).attr('id'));
-	})
-});
+// 	$(".link", this).on('click', function() {
+// 		showText($(this).attr('id'));
+// 	})
+// });
